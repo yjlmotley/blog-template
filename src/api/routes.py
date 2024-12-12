5cebc6cd5a8b7b6ee41f4cd7f7b3cc157ea476ce
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-12-12 22:49:05 +0000

    cleaning up rando files from boilerplate

--- src/api/routes.py
+++ src/api/routes.py
@@ -12,11 +12,11 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/testing', methods=['POST', 'GET'])
 def handle_hello():
 
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Hello! This is for testing purposes. I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
     return jsonify(response_body), 200
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,18 +2,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
 			userId: localStorage.getItem("userId") || 999,
 			blogs: [],
 			blogError: null,
@@ -22,15 +10,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 			userLikeStatus: null,
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
 
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
+					const resp = await fetch(process.env.BACKEND_URL + "/api/testing")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
